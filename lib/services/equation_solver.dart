3ade501048f7a9e8dca59f245c35d8d1e721ac17
Williams Lenkeu <williams25prince@gmail.com>
2024-05-24 10:17:11 +0100

    update

--- lib/services/equation_solver.dart
+++ lib/services/equation_solver.dart
@@ -1,43 +1,43 @@
 import 'dart:math' as math;
 
-num cbrt(num x) {
+num cubicRoot(num x) {
   return x < 0 ? -math.pow(-x, 1/3) : math.pow(x, 1/3);
 }
 
-List<num> solveCubicEquation(num a, num b, num c, num d) {
-  num f = ((3 * c / a) - ((b * b) / (a * a))) / 3;
-  num g = ((2 * (b * b * b) / (a * a * a)) - (9 * b * c / (a * a)) + (27 * d / a)) / 27;
-  num h = ((g * g) / 4) + ((f * f * f) / 27);
+List<num> solveCubicEquation(num coefficientA, num coefficientB, num coefficientC, num coefficientD) {
+  num termF = ((3 * coefficientC / coefficientA) - ((coefficientB * coefficientB) / (coefficientA * coefficientA))) / 3;
+  num termG = ((2 * (coefficientB * coefficientB * coefficientB) / (coefficientA * coefficientA * coefficientA)) - (9 * coefficientB * coefficientC / (coefficientA * coefficientA)) + (27 * coefficientD / coefficientA)) / 27;
+  num termH = ((termG * termG) / 4) + ((termF * termF * termF) / 27);
 
-  // Ici j'affecte la valeur du calcul du discriminant pour déterminer le nombre et le type de solutions
-  num discriminant = h;
+  // Calcul du discriminant pour déterminer le nombre et le type de solutions
+  num discriminant = termH;
 
   if (discriminant > 0) {
     // Une solution réelle et deux solutions complexes
-    num r = -(g / 2) + math.sqrt(h);
-    num s = cbrt(r);
-    num t = -(g / 2) - math.sqrt(h);
-    num u = cbrt(t);
-    num x1 = (s + u) - (b / (3 * a));
-    return [x1.toDouble()];
+    num r = -(termG / 2) + math.sqrt(termH);
+    num s = cubicRoot(r);
+    num t = -(termG / 2) - math.sqrt(termH);
+    num u = cubicRoot(t);
+    num solution = (s + u) - (coefficientB / (3 * coefficientA));
+    return [solution.toDouble()];
   } else if (discriminant == 0) {
-    // Trois solutions réelles identiques ou deux réelles (one being double)
-    num u1 = cbrt(d / a);
-    num x1 = -2 * u1 - (b / (3 * a));
-    num x2 = u1 - (b / (3 * a));
-    return [x1.toDouble(), x2.toDouble(), x2.toDouble()];
+    // Trois solutions réelles identiques ou deux réelles (l'une étant double)
+    num u1 = cubicRoot(coefficientD / coefficientA);
+    num solution1 = -2 * u1 - (coefficientB / (3 * coefficientA));
+    num solution2 = u1 - (coefficientB / (3 * coefficientA));
+    return [solution1.toDouble(), solution2.toDouble(), solution2.toDouble()];
   } else {
     // Trois solutions réelles différentes
-    num i = math.sqrt((g * g) / 4 - h);
-    num j = cbrt(i);
-    num k = math.acos(-(g / (2 * i)));
+    num i = math.sqrt((termG * termG) / 4 - termH);
+    num j = cubicRoot(i);
+    num k = math.acos(-(termG / (2 * i)));
     num l = -j;
     num m = math.cos(k / 3);
     num n = math.sqrt(3) * math.sin(k / 3);
-    num p = -(b / (3 * a));
-    num x1 = 2 * j * math.cos(k / 3) - (b / (3 * a));
-    num x2 = l * (m + n) + p;
-    num x3 = l * (m - n) + p;
-    return [x1.toDouble(), x2.toDouble(), x3.toDouble()];
+    num p = -(coefficientB / (3 * coefficientA));
+    num solution1 = 2 * j * math.cos(k / 3) - (coefficientB / (3 * coefficientA));
+    num solution2 = l * (m + n) + p;
+    num solution3 = l * (m - n) + p;
+    return [solution1.toDouble(), solution2.toDouble(), solution3.toDouble()];
   }
 }
